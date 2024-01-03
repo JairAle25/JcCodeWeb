b9d699077704fa3d9a62c90d32b8c12bf13c9c18
Jair Cornejo <jaircornejo61@gmail.com>
2024-01-03 13:12:50 -0300

    hice la barra de navegacion responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,33 @@
 import logo from '../../imgs/jccodeweb.png';
 import './header.css';
 import { links } from '../../constants/links';
+import { List } from 'react-bootstrap-icons';
+import { useState } from 'react';
 
 const Header = () => {
+    const [classNameList,setClassNameList] = useState("listaResponsive");
+
+    const [classNameLinks,setClassNameLinks] = useState("links");
+
+    const RotarLista =()=>{
+        if(classNameList === "listaResponsive"){
+            setClassNameList("listaResponsive rotarLista");
+        }
+        else{
+            setClassNameList("listaResponsive");
+        }
+        mostrarLinks();
+    }
+
+    const mostrarLinks=()=>{
+        if(classNameLinks === "links"){
+            setClassNameLinks("links mostrarLista");
+        }
+        else{
+            setClassNameLinks("links");
+        }
+    }
+
     return ( 
         <>
             <header className='cabecera'>
@@ -10,9 +35,10 @@ const Header = () => {
                     <a href=""><img src={logo} alt="" /></a>
                 </div>
                 
-                <nav className='links'>
+                <nav className={classNameLinks}>
                      {links.map(link => <a key={link.id} href={link.url}>{link.name}</a>)}
                 </nav>
+                <List className={classNameList} onClick={RotarLista}></List>
             </header>
         </>
      );
